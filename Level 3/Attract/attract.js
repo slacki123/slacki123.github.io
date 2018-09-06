33fe9e8c336a99a1bf07c5e5fdd8e285c24f5956
zinmoenaung <zinmoenaung@gmail.com>
2018-09-06 17:18:40 +0100

    ll

--- Level 3/Attract/attract.js
+++ Level 3/Attract/attract.js
@@ -1,8 +1,10 @@
-var gravity = 0.5;
+var mode = "planet";
+
+var gravity = 0.4;
 var yVelocity = 0;
 var xVelocity = 0;
 var magnetAccel = 0.001;
-var damper = 0.01;
+var damper = 0.02;
 
 
 var xPos = 0;
@@ -14,14 +16,11 @@ var anvilY = document.getElementById("anvil");
 anvilX.style.left = "200px";
 anvilY.style.top =  "200px";
 
+var anvilPosX;
+var anvilPosY;
+var xDiffer;
+var yDiffer;
 
-
-
-function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve , ms));
-}
-
-//await sleep(time..)
 
 
 function mousePosition(event){
@@ -34,38 +33,96 @@ function mousePosition(event){
 }
 
 
-setInterval(attract, 10);
+onload = function() {
+	setInterval(attract, 16);
+	
+}
+
+function weirdSmoothingFunction(){
+	
+	anvilPosY = anvilPosY + yVelocity; anvilPosY = anvilPosY + yVelocity;
+	anvilPosX = anvilPosX + xVelocity; anvilPosX = anvilPosX + xVelocity;anvilPosY = anvilPosY + yVelocity; anvilPosY = anvilPosY + yVelocity;
+	anvilPosX = anvilPosX + xVelocity; anvilPosX = anvilPosX + xVelocity;anvilPosY = anvilPosY + yVelocity; anvilPosY = anvilPosY + yVelocity;
+	anvilPosX = anvilPosX + xVelocity; anvilPosX = anvilPosX + xVelocity;anvilPosY = anvilPosY + yVelocity; anvilPosY = anvilPosY + yVelocity;
+	anvilPosX = anvilPosX + xVelocity; anvilPosX = anvilPosX + xVelocity;anvilPosY = anvilPosY + yVelocity; anvilPosY = anvilPosY + yVelocity;
+	anvilPosX = anvilPosX + xVelocity; anvilPosX = anvilPosX + xVelocity;anvilPosY = anvilPosY + yVelocity; anvilPosY = anvilPosY + yVelocity;
+	anvilPosX = anvilPosX + xVelocity; anvilPosX = anvilPosX + xVelocity;
+	
+}
+
+
+function drawSpring(){
+
+	document.getElementById("butt1").style.top = yPos + (1/8)*yDiffer + "px";
+	document.getElementById("butt1").style.left = xPos + (1/8)*xDiffer + "px";
+	
+	document.getElementById("butt2").style.top = yPos + (2/8)*yDiffer + "px";
+	document.getElementById("butt2").style.left = xPos + (2/8)*xDiffer + "px";
+	
+	document.getElementById("butt3").style.top = yPos + (3/8)*yDiffer + "px";
+	document.getElementById("butt3").style.left = xPos + (3/8)*xDiffer + "px";
+	
+	document.getElementById("butt4").style.top = yPos + (4/8)*yDiffer + "px";
+	document.getElementById("butt4").style.left = xPos + (4/8)*xDiffer + "px";
+
+	document.getElementById("butt5").style.top = yPos + (5/8)*yDiffer + "px";
+	document.getElementById("butt5").style.left = xPos + (5/8)*xDiffer + "px";
+
+	document.getElementById("butt6").style.top = yPos + (6/8)*yDiffer + "px";
+	document.getElementById("butt6").style.left = xPos + (6/8)*xDiffer + "px";
+	
+	document.getElementById("butt7").style.top = yPos + (7/8)*yDiffer + "px";
+	document.getElementById("butt7").style.left = xPos + (7/8)*xDiffer + "px";
+
+
+}
+
+function planetMode(){
+	
+	xAcceleration = (xAcceleration/(Math.sqrt(yAcceleration**2+xAcceleration**2)));
+	yAcceleration = (yAcceleration/(Math.sqrt(yAcceleration**2+xAcceleration**2)));
+	magnetAccel = 0.01;
+	gravity = 0;
+	damper = 0.001;
+}
 
 
 function attract(){
 	
 	document.getElementById("magnetPic").hidden = false;
 	
-	var anvilPosX = parseInt(anvilX.style.left.substring(0, anvilX.style.left.length-2));
-	var anvilPosY = parseInt(anvilY.style.top.substring(0, anvilY.style.top.length-2))
+	 anvilPosX = parseInt(anvilX.style.left.substring(0, anvilX.style.left.length-2));
+	 anvilPosY = parseInt(anvilY.style.top.substring(0, anvilY.style.top.length-2))
 	
 
-	var xDiffer = anvilPosX - xPos;
-	var yDiffer = anvilPosY - yPos;
-	
-//	gravity = 0;
+	 xDiffer = anvilPosX - xPos;
+	 yDiffer = anvilPosY - yPos;
 	
 	yVelocity = yVelocity + gravity;
 	
-	//for some reason the thing below makes a difference. Does it just smoothen things?
-	anvilPosY = anvilPosY + yVelocity;
-	anvilPosX = anvilPosX + xVelocity;
+	//for some reason makes a difference?
+	weirdSmoothingFunction();
 	
-//	console.log(yVelocity);
 	
-	//slinglike
-	var yAcceleration = Math.abs(yDiffer)
-	var xAcceleration = Math.abs(xDiffer)
+	var yAcceleration = Math.abs(yDiffer);
+	var xAcceleration = Math.abs(xDiffer);
 	
-//	//planetLike
-//	yAcceleration = Math.abs(1/(yDiffer+xDiffer))
-//	xAcceleration = Math.abs(1/(yDiffer+xDiffer))
-//	
+	if(mode == "spring"){
+		
+	
+	drawSpring();
+		
+	}
+	
+
+	xAcceleration = (xAcceleration/(Math.sqrt(yAcceleration**2+xAcceleration**2)));
+	yAcceleration = (yAcceleration/(Math.sqrt(yAcceleration**2+xAcceleration**2)));
+	magnetAccel = 0.01;
+	gravity = 0;
+	damper = 0.001;
+		
+	
+	
 	if(anvilPosY> 1500){
 		anvilPosY = 0;
 		anvilPosX = 200; 
@@ -76,40 +133,25 @@ function attract(){
 	
 
 	if (yDiffer > 0) {
-		yVelocity = yVelocity - magnetAccel* yAcceleration -yVelocity*damper;
-		anvilPosY = anvilPosY + yVelocity;
-		
+		yVelocity = yVelocity - magnetAccel * yAcceleration - yVelocity*damper;
+		anvilPosY = anvilPosY + yVelocity;		
 	}
 	
 	if (yDiffer < 0) {
-		yVelocity = yVelocity + magnetAccel*yAcceleration-yVelocity*damper;
-		anvilPosY = anvilPosY + yVelocity;
-		
+		yVelocity = yVelocity + magnetAccel * yAcceleration - yVelocity*damper;
+		anvilPosY = anvilPosY + yVelocity;		
 	}
 	
 	if (xDiffer < 0){
-		xVelocity = xVelocity + magnetAccel*xAcceleration-xVelocity*damper ;
+		xVelocity = xVelocity + magnetAccel * xAcceleration - xVelocity*damper;
 		anvilPosX = anvilPosX + xVelocity;
 	}
 	
 	
 	if (xDiffer > 0) {
-		xVelocity = xVelocity - magnetAccel*xAcceleration -xVelocity*damper;
+		xVelocity = xVelocity - magnetAccel * xAcceleration - xVelocity*damper;
 		anvilPosX = anvilPosX + xVelocity;
 	}
-	
-//	console.log(anvilPosY);
-	
-	/* MIND
-	
-	1/xDiffer is like planets
-	xDiffer is like being attached to a sling
-	
-	should have had x and y for accelleration too
-	
-	*/
-		
-	
 	
 
 	var changeX =  anvilPosX + "px";
@@ -123,13 +165,8 @@ function attract(){
 	document.getElementById("magnetPic").style.left = xPos + "px";
 	document.getElementById("magnetPic").style.top = yPos + "px";
 	
-//	document.getElementById("butt").style.top = xDiffer/2 + "px";
-//	console.log(document.getElementById("butt").style.top);
-
-
-	
-
 	
 	
+
 }
 
