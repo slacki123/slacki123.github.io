545c3e69df1516dc6f8a9572cba8810166b385d2
slacki123 <sebastian.lacki1@gmail.com>
2018-11-26 19:42:45 +0000

    structured content

--- LivePerson stuff/structuredContent.js
+++ LivePerson stuff/structuredContent.js
@@ -6,43 +6,32 @@ function sendRichContent() {
 
   lpTag.agentSDK.init();
 
-  // cmdName = lpTag.agentSDK.cmdNames.writeSC; // = "Write StructuredContent"
-  //   data = {
-  //     json: {
-  //       "text": "yourBusinessName",
-  //       "type": "vertical",
-  //       "elements": [
-  //         {
-  //           "type": "map",
-  //           "lo": "yourLongitude",
-  //           "la": "yourLatitude",
-  //           "tooltip": "Map",
-  //           "click": {
-  //             "actions": [
-  //               {
-  //                 "type": "navigate",
-  //                 "name": "navigate",
-  //                 "lo": "yourLongitude",
-  //                 "la": "yourLatitude"
-  //               }  
-  //             ]
-  //           }
-  //         },
-  //         {
-  //           "type": "text",
-  //           "text": "1234 Hollywood Boulevard, Los Angeles, CA"
-  //         },
-  //       ],
-  //       "metadata": { "fallback": "url_link_map_link" }
-  //     },
-  //     metadata: [ //metadata is optional
-  //         {"type":"ExternalId","id":"running364"},
-  //         {"type":"ExternalId","id":"soccer486"}
-  //       ]
-  //   };
+  cmdName = lpTag.agentSDK.cmdNames.writeSC; // = "Write StructuredContent"
+    data = {
+      "json":  {
+        "type": "vertical",
+          "elements": 
+            [{
+              "type": "image",
+              "url": "https://s0.geograph.org.uk/geophotos/04/17/37/4173703_d031288b.jpg",
+              "tooltip": "image tooltip"
+            }, {
+              "type": "text",
+              "text": "product name (The picture above)",
+              "tooltip": "product name (Title)"
+            }, {
+              "type": "text",
+              "text": "product category (Any)",
+              "tooltip": "product category (type)"
+            }, {
+              "type": "text",
+              "text": "$155.99",
+              "tooltip": "$155.99"
+            }]
+    };
 
-    cmdName = lpTag.agentSDK.cmdNames.write; // = "Write ChatLine"
-    data = {text: "WORKS MAFAKA"};
+    // cmdName = lpTag.agentSDK.cmdNames.write; // = "Write ChatLine"
+    // data = {text: "WORKS MAFAKA"};
 
     var notifyWhenDone = function(err) {
         if (err) {
