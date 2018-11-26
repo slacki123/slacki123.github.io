6e60f11f80431dcac3bf2defac06aa27cf8fb4ab
slacki123 <sebastian.lacki1@gmail.com>
2018-11-26 19:29:57 +0000

    work pls

--- LivePerson stuff/structuredContent.js
+++ LivePerson stuff/structuredContent.js
@@ -5,41 +5,44 @@ var data;
 function sendRichContent() {
 
   lpTag.agentSDK.init();
-  
-  cmdName = lpTag.agentSDK.cmdNames.writeSC; // = "Write StructuredContent"
-    data = {
-      json: {
-        "text": "yourBusinessName",
-        "type": "vertical",
-        "elements": [
-          {
-            "type": "map",
-            "lo": "yourLongitude",
-            "la": "yourLatitude",
-            "tooltip": "Map",
-            "click": {
-              "actions": [
-                {
-                  "type": "navigate",
-                  "name": "navigate",
-                  "lo": "yourLongitude",
-                  "la": "yourLatitude"
-                }  
-              ]
-            }
-          },
-          {
-            "type": "text",
-            "text": "1234 Hollywood Boulevard, Los Angeles, CA"
-          },
-        ],
-        "metadata": { "fallback": "url_link_map_link" }
-      },
-      metadata: [ //metadata is optional
-          {"type":"ExternalId","id":"running364"},
-          {"type":"ExternalId","id":"soccer486"}
-        ]
-    };
+
+  // cmdName = lpTag.agentSDK.cmdNames.writeSC; // = "Write StructuredContent"
+  //   data = {
+  //     json: {
+  //       "text": "yourBusinessName",
+  //       "type": "vertical",
+  //       "elements": [
+  //         {
+  //           "type": "map",
+  //           "lo": "yourLongitude",
+  //           "la": "yourLatitude",
+  //           "tooltip": "Map",
+  //           "click": {
+  //             "actions": [
+  //               {
+  //                 "type": "navigate",
+  //                 "name": "navigate",
+  //                 "lo": "yourLongitude",
+  //                 "la": "yourLatitude"
+  //               }  
+  //             ]
+  //           }
+  //         },
+  //         {
+  //           "type": "text",
+  //           "text": "1234 Hollywood Boulevard, Los Angeles, CA"
+  //         },
+  //       ],
+  //       "metadata": { "fallback": "url_link_map_link" }
+  //     },
+  //     metadata: [ //metadata is optional
+  //         {"type":"ExternalId","id":"running364"},
+  //         {"type":"ExternalId","id":"soccer486"}
+  //       ]
+  //   };
+
+    cmdName = lpTag.agentSDK.cmdNames.write; // = "Write ChatLine"
+    data = {text: "WORKS MAFAKA"};
 
     var notifyWhenDone = function(err) {
         if (err) {
