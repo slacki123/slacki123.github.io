b8bafa2be0ab4068860aec9b1c6f8c78747b5b78
slacki123 <sebastian.lacki1@gmail.com>
2019-01-02 16:47:08 +0000

    wtf

--- LivePerson stuff/structuredContent.js
+++ LivePerson stuff/structuredContent.js
@@ -50,7 +50,7 @@ data =
 				}
 			}]
 		},
-		
+
 	{
       "type": "vertical",
       "elements": [
@@ -212,3 +212,76 @@ function sendRichContent() {
 
 
 }
+
+function sendQuickReplies() {
+
+   cmdName = lpTag.agentSDK.cmdNames.writeSC; // = "Write StructuredContent"
+    data = {
+      "text": "Some text",
+      "quickReplies": {
+        "type": "quickReplies",
+        "itemsPerRow": 8,
+        "replies": [
+          {
+            "type": "button",
+            "tooltip": "yes i do",
+            "title": "yes",
+            "click": {
+              "actions": [
+                {
+                  "type": "publishText",
+                  "text": "yep"
+                }
+              ],
+              "metadata": [
+                {
+                  "type": "ExternalId",
+                  "id": "Yes-1234"
+                }
+              ]
+            }
+          },
+          {
+            "type": "button",
+            "tooltip": "No!",
+            "title": "No!",
+            "click": {
+              "actions": [
+                {
+                  "type": "publishText",
+                  "text": "No!"
+                }
+              ],
+              "metadata": [
+                {
+                  "type": "ExternalId",
+                  "id": "No-4321"
+                }
+              ]
+            }
+          }
+        ]
+      }
+    };
+
+    // cmdName = lpTag.agentSDK.cmdNames.write; // = "Write ChatLine"
+    // data = {text: "WORKS MAFAKA"};
+
+    var notifyWhenDone = function(err) {
+        if (err) {
+            // Do something with the error
+            console.log(err);
+        }
+        // called when the command is completed successfully,
+        // or when the action terminated with an error.
+    };
+
+   
+
+      try {
+        lpTag.agentSDK.command(cmdName, data, notifyWhenDone);
+
+        } catch (e) {
+          console.log(e);
+        }
+ }
