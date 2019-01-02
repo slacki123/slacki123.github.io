84f4dd304ba7e08f01e1dcd7ca7f986fd118f6ee
slacki123 <sebastian.lacki1@gmail.com>
2019-01-02 16:52:21 +0000

    added quickreplies

--- LivePerson stuff/structuredContent.js
+++ LivePerson stuff/structuredContent.js
@@ -215,7 +215,7 @@ function sendRichContent() {
 
 function sendQuickReplies() {
 
-   cmdName = lpTag.agentSDK.cmdNames.writeSC; // = "Write StructuredContent"
+   cmdName = lpTag.agentSDK.cmdNames.write; // = "Write StructuredContent"
     data = {
       "text": "Some text",
       "quickReplies": {
@@ -263,6 +263,88 @@ function sendQuickReplies() {
         ]
       }
     };
+
+    // cmdName = lpTag.agentSDK.cmdNames.write; // = "Write ChatLine"
+    // data = {text: "WORKS MAFAKA"};
+
+    var notifyWhenDone = function(err) {
+        if (err) {
+            // Do something with the error
+            console.log(err);
+        }
+        // called when the command is completed successfully,
+        // or when the action terminated with an error.
+    };
+
+   
+
+      try {
+        lpTag.agentSDK.command(cmdName, data, notifyWhenDone);
+
+        } catch (e) {
+          console.log(e);
+        }
+ }
+
+ function sendQuickRepliesAsSC() {
+
+   cmdName = lpTag.agentSDK.cmdNames.writeSC; // = "Write StructuredContent"
+var data = {
+      json: {
+        "type": "text",
+        "text": "product name",
+        "tooltip": "text tooltip",
+        "style": {
+          "bold": true,
+          "size": "large"
+        }
+      },
+      quickReplies: {
+        "type": "quickReplies",
+        "itemsPerRow": 8,
+        "replies": [
+          {
+            "type": "button",
+            "tooltip": "yes i do",
+            "title": "yes",
+            "click": {
+              "actions": [
+                {
+                  "type": "publishText",
+                  "text": "yep"
+                }
+              ],
+              "metadata": [
+                {
+                  "type": "ExternalId",
+                  "id": "Yes-1234"
+                }
+              ]
+            }
+          },
+          {
+            "type": "button",
+            "tooltip": "No!",
+            "title": "No!",
+            "click": {
+              "actions": [
+                {
+                  "type": "publishText",
+                  "text": "No!"
+                }
+              ],
+              "metadata": [
+                {
+                  "type": "ExternalId",
+                  "id": "No-4321"
+                }
+              ]
+            }
+          }
+        ]
+      }
+   };
+
 
     // cmdName = lpTag.agentSDK.cmdNames.write; // = "Write ChatLine"
     // data = {text: "WORKS MAFAKA"};
