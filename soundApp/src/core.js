80b0cd676c079241b411035590578cfba47d29a2
slacki123 <sebastian.lacki1@gmail.com>
2020-05-29 21:13:48 +0100

    replaced original birds recording cause was crap

--- soundApp/src/core.js
+++ soundApp/src/core.js
@@ -3,7 +3,6 @@ const myAudio = document.getElementById('myAudio');
 const fadeTypeDropdown = document.getElementById('fadeDropdown');
 const fadeDurationInput = document.getElementById('fadeDurationInput');
 const myAudioSounds = [
-    'birdSounds/Birds.m4a',
     'birdSounds/Birds 2.m4a',
     'birdSounds/Birds one cough and fly.m4a',
     'birdSounds/Birds talk at end.m4a',
@@ -12,7 +11,8 @@ const myAudioSounds = [
     'birdSounds/Birds with neighbours talking.m4a',
     'birdSounds/Birds with weird pigeon noise.m4a',
     'birdSounds/Birds from inside.m4a',
-    'birdSounds/Birds and a crow.m4a'
+    'birdSounds/Birds and a crow.m4a',
+    'birdSounds/Birds from inside 2.m4a'
 ];
 setDefaultValues();
 
